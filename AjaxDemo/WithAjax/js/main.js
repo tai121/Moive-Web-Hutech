1dee3bb7a43b6477540c041b7583e2495c89957d
truongnhon-hutech <vothuongtruongnhon2002@gmail.com>
2022-05-26 09:58:47 +0700

    demo ajax

--- AjaxDemo/WithAjax/js/main.js
+++ AjaxDemo/WithAjax/js/main.js
@@ -2,8 +2,7 @@ function clickHandler() {
     let xhr = new XMLHttpRequest();
 
     xhr.open('GET', "Server/sayHello.txt", true)
-
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         console.log(xhr.readyState)
         if (xhr.readyState == 3) {
             var result = document.getElementById('result')
@@ -15,4 +14,95 @@ function clickHandler() {
         // result.innerHTML = xhr.responseText
     }
     xhr.send();
+}
+// ------------------------------------------------------------
+// userinterface tao event roi tao doi tuong xmlhttprequest gui len server  
+// roi server tra ve ket qua 
+// luu du lieu lai vao file 
+// load lai trang web
+// ------------------------------------------------------------
+//**submit get data save it into txt file then show them in page but add load fuction */
+// ------------------------------------------------------------
+function clickHandlerButLoad() {
+    var resultNotAjax = document.getElementById('resultNotAjax');
+    // var txt = document.createTextNode("Hello World!!!");
+    // resultNotAjax.appendChild(txt);
+    resultNotAjax.innerHTML = "Hello World";
+    resultNotAjax.style.color = "red";
+    setTimeout(() => {
+        window.location.reload();
+    }, 2000);
+}
+
+// ------------------------------------------------------------
+// cd index.html
+// ------------------------------------------------------------
+
+
+// ------------------------------------------------------------
+// cd left right
+var i = 0;
+var x;
+displayCD(i);
+
+function displayCD(i) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this, i);
+        }
+    };
+    xmlhttp.open("GET", "cd_catalog.xml", true);
+    xmlhttp.send();
+}
+
+function myFunction(xml, i) {
+    var xmlDoc = xml.responseXML;
+    x = xmlDoc.getElementsByTagName("CD");
+    document.getElementById("showCD").innerHTML =
+        "Artist: " +
+        x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+        "<br>Title: " +
+        x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+        "<br>Year: " +
+        x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
+}
+
+function next() {
+    if (i < x.length - 1) {
+        i++;
+        displayCD(i);
+    }
+}
+
+function previous() {
+    if (i > 0) {
+        i--;
+        displayCD(i);
+    }
+}
+// ------------------------------------------------------------
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunctionLoad(this);
+        }
+    };
+    xmlhttp.open("GET", "cd_catalog.xml", true);
+    xmlhttp.send();
+}
+function myFunctionLoad(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = "<tr><th>Artist</th><th>Title</th></tr>";
+    var x = xmlDoc.getElementsByTagName("CD");
+    for (i = 0; i < x.length; i++) {
+        table += "<tr><td>" +
+            x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+            "</td></tr>";
+    }
+    document.getElementById("demo").innerHTML = table;
 }
